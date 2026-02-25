d791662d9cd89eb6530eddedbba040ffba7ba8db
Pang <xsasis0040@gmail.com>
2026-02-25 16:19:31 +0700

    up date

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
-import { Kanit } from "next/font/google"; // Next.js จะโหลด Font ให้เองอัตโนมัติ
+import { Kanit } from "next/font/google";
 import "./globals.css";
+import Script from "next/script"; // 1. เพิ่มตัวเรียกใช้ Script ของ Next.js
 
 const kanit = Kanit({ 
   subsets: ["thai", "latin"], 
   weight: ["300", "400", "500", "600", "700"],
-  variable: "--font-kanit", // สร้างเป็น Variable ไว้ใช้ใน Tailwind
+  variable: "--font-kanit", 
 });
 
 export default function RootLayout({
@@ -15,13 +16,27 @@ export default function RootLayout({
   return (
     <html lang="th">
       <head>
-        {/* ✅ เพิ่ม Link นี้เข้าไปเพื่อให้เรียกใช้ class="fas fa-..." ได้เหมือนเดิมค่ะ */}
+        {/* Font Awesome เดิมของปัง */}
         <link 
           rel="stylesheet" 
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" 
         />
+
+        {/* ✅ เพิ่ม TikTok Pixel Code ตรงนี้ค่ะ */}
+        <Script id="tiktok-pixel" strategy="afterInteractive">
+          {`
+            !function (w, d, t) {
+              w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+              var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+              ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+              ttq.load('D6FBPKBC77UEP1KMRJVG'); // ⚠️ ปังอย่าลืมเช็ค ID ตรงนี้ให้ตรงกับใน TikTok Ads Manager น่อ
+              ttq.page();
+            }(window, document, 'ttq');
+          `}
+        </Script>
       </head>
-      <body className="...">
+      <body className={kanit.className}>
         {children}
       </body>
     </html>
